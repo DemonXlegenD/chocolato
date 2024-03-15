87d2cfaf1330f9cfcd7e803dbdf39d9fd21812b1
quentin lf <quentinleforestier0@gmail.com>
2024-03-15 22:58:35 +0100

    tkt

--- Assets/Scripts/PoolObjects.cs
+++ Assets/Scripts/PoolObjects.cs
@@ -7,14 +7,30 @@ public class PoolObjects : MonoBehaviour
 {
     [SerializeField] int maxEnemies;
     [SerializeField] int maxCookies;
+    [SerializeField] int maxVague1;
+    [SerializeField] int maxVague2;
+    [SerializeField] int maxVague3;
+    [SerializeField] int maxVague4;
     [SerializeField] int maxenemyBullets;
     private List<GameObject> enemies;
     private List<GameObject> enemyBullets;
     private List<GameObject> lootCookiesBlack;
     private List<GameObject> lootCookiesWhite;
+    private List<GameObject> vague1;
+    private List<GameObject> vague2;
+    private List<GameObject> vague3;
+    private List<GameObject> vague4;
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] GameObject cookieBlackPrefab;
     [SerializeField] GameObject cookieWhitePrefab;
+    [SerializeField] GameObject basicWhite;
+    [SerializeField] GameObject basicBlack;
+    [SerializeField] GameObject regnedWhite;
+    [SerializeField] GameObject regnedBlack;
+    [SerializeField] GameObject kamikazeWhite;
+    [SerializeField] GameObject kamikazeBlack;
+    [SerializeField] GameObject diggerWhite;
+    [SerializeField] GameObject diggerBlack;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +38,10 @@ public class PoolObjects : MonoBehaviour
         enemyBullets = new List<GameObject>();
         lootCookiesBlack = new List<GameObject>();
         lootCookiesWhite = new List<GameObject>();
+        lootCookiesWhite = new List<GameObject>();
+        lootCookiesWhite = new List<GameObject>();
+        lootCookiesWhite = new List<GameObject>();
+
         for (int i = 0; i < maxenemyBullets; i++)
         {
             GameObject bullet = Instantiate(bulletPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
@@ -30,17 +50,122 @@ public class PoolObjects : MonoBehaviour
         }
         for (int i = 0; i < maxCookies; i++)
         {
-            if(i < maxCookies/2) 
+            if (i < maxCookies / 2)
             {
                 GameObject cookie = Instantiate(cookieBlackPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
                 cookie.SetActive(false);
                 lootCookiesBlack.Add(cookie);
             }
-            else 
+            else
             {
                 GameObject cookie = Instantiate(cookieWhitePrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
                 cookie.SetActive(false);
                 lootCookiesWhite.Add(cookie);
+            }
+        }
+        for (int i = 0; i < maxVague1; i++)
+        {
+            if (i < maxVague1 / 2)
+            {
+                GameObject enemiVague1 = Instantiate(basicWhite, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+            }
+            else
+            {
+                GameObject enemiVague1 = Instantiate(basicBlack, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+            }
+        }
+        for (int i = 0; i < maxVague2; i++)
+        {
+            int tmp = maxVague2 / 4;
+            if (i < tmp)
+            {
+                GameObject enemiVague1 = Instantiate(basicWhite, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+
+            }
+            else if (i < tmp * 2)
+            {
+                GameObject enemiVague1 = Instantiate(basicBlack, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+            }
+            else if (i < tmp * 3)
+            {
+                GameObject enemiVague1 = Instantiate(regnedWhite, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+            }
+
+            else
+            {
+                GameObject enemiVague1 = Instantiate(regnedBlack, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+            }
+        }
+        for (int i = 0; i < maxVague3; i++)
+        {
+            int tmp = maxVague3 / 4;
+            if (i < tmp)
+            {
+                GameObject enemiVague1 = Instantiate(basicWhite, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+
+            }
+            else if (i < tmp * 2)
+            {
+                GameObject enemiVague1 = Instantiate(basicBlack, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+            }
+            else if (i < tmp * 3)
+            {
+                GameObject enemiVague1 = Instantiate(regnedWhite, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+            }
+
+            else
+            {
+                GameObject enemiVague1 = Instantiate(regnedBlack, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+            }
+        }
+        for (int i = 0; i < maxVague4; i++)
+        {
+            int tmp = maxVague4 / 4;
+            if (i < tmp)
+            {
+                GameObject enemiVague1 = Instantiate(basicWhite, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+
+            }
+            else if (i < tmp * 2)
+            {
+                GameObject enemiVague1 = Instantiate(basicBlack, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+            }
+            else if (i < tmp * 3)
+            {
+                GameObject enemiVague1 = Instantiate(regnedWhite, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
+            }
+
+            else
+            {
+                GameObject enemiVague1 = Instantiate(regnedBlack, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                enemiVague1.SetActive(false);
+                vague1.Add(enemiVague1);
             }
         }
     }
