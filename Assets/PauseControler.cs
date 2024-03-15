50b91ff3c3d5a9b715565ee903a2b9f92809c57b
Maxime.C <macorona.41@gmail.com>
2024-03-15 17:34:40 +0100

    Pause controler

--- Assets/PauseControler.cs
+++ Assets/PauseControler.cs
@@ -8,12 +8,6 @@ public class PauseControler : MonoBehaviour
     public UnityEvent GamePaused;
     public UnityEvent GameResumed;
     private bool _isPaused;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
